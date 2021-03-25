7d89fbd9d63faf4b8a6d0a114109b2a6c0f7dbc8
Christoph Reiter <reiter.christoph@gmail.com>
2021-03-25 11:31:09 +0100

    Missing file

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBP\API\AuthenticDocumentBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('dbp_authentic_document');
+
+        $treeBuilder->getRootNode()
+                ->children()
+                    ->scalarNode('co_oauth2_api_client_id')->end()
+                    ->scalarNode('co_oauth2_api_client_secret')->end()
+                    ->scalarNode('co_oauth2_api_api_url')->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
